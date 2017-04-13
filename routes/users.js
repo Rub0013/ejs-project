302fb338e65bd9b9c496ab03c564247e6df1e37e
Rub <rubenallaverdyan13@gmail.com>
2017-04-13 23:13:28 +0400

    First

---
+++ routes/users.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var users = {};
+request('https://randomuser.me/api/', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        users = JSON.parse(body).results;
+        console.log(users);
+    }
+});
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.render('users', {
+        title: 'Users',
+        users: users
+    });
+});
+
+module.exports = router;
