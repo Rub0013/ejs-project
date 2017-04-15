5d4cb96301cb438d9a65555dcb9949f2998c5f0e
Rub0013 <rubenallaverdyan13@gmail.com>
2017-04-15 15:43:29 +0400

    MongoDB

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
+var assert = require('assert');
+var url = 'mongodb://localhost:27017/test';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home', success: req.session.success, errors: req.session.errors});
-  req.session.errors = false;
+  mongo.connect(url, function (err, db) {
+      var allUsers = [];
+      assert.equal(null, err);
+      var cursor = db.collection('users').find();
+      cursor.forEach(function (doc, err) {
+          assert.equal(null, err);
+          allUsers.push(doc);
+      }, function () {
+          db.close();
+          res.render('index', {
+              title: 'Home',
+              users: allUsers,
+              success: req.session.success,
+              errors: req.session.errors
+          });
+          req.session.errors = false;
+      });
+  });
 });
 
 router.get('/test/:id', function(req, res, next) {
@@ -15,7 +35,7 @@ router.get('/test/:id', function(req, res, next) {
 });
 
 // Post validation.
-router.post('/submit', function(req, res, next) {
+router.post('/insert', function(req, res, next) {
     req.check('name', 'Write your name!').notEmpty();
     req.check('email', 'Invalid email address!').isEmail();
     req.check('number', 'Phone number must contain only numbers!').isNumeric();
@@ -26,9 +46,35 @@ router.post('/submit', function(req, res, next) {
         req.session.success = false;
     }
     else{
+        var user = {
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.number,
+        };
+        mongo.connect(url, function (err, db) {
+            assert.equal(null, err);
+            db.collection('users').insertOne(user, function (err, result) {
+                assert.equal(null, err);
+                console.log('Item inserted!');
+                db.close();
+            });
+        });
         req.session.success = true;
     }
     res.redirect('/');
+});
+router.post('/update', function(req, res, next) {});
+router.post('/delete', function(req, res, next) {
+    var id = req.body.id;
+    res.send('ok');
+    // mongo.connect(url, function (err, db) {
+    //     assert.equal(null, err);
+    //     db.collection('users').deleteOne({"_id":objectId(id)}, function (err, result) {
+    //         assert.equal(null, err);
+    //         console.log('Item deleted!');
+    //         db.close();
+    //     });
+    // });
 });
 
 module.exports = router;
